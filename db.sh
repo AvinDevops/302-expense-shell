c36050bc180f81e079002a479716f9880907888e
AvinDevops <avinashbandela73@gmail.com>
2026-05-09 23:36:44 +0530

    db

--- db.sh
+++ db.sh
@@ -4,6 +4,10 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPTNAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
 
+#prompt for root password
+echo -e "$Y please enter root password $N"
+read MYSQL_ROOT_PASSWD
+
 #colors
 R="\e[31m"
 G="\e[32m"
@@ -41,5 +45,15 @@ VALIDATE $? "Enabiling mysqld"
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Starting mysqld"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-VALIDATE $? "Setting password for root"
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+# VALIDATE $? "Setting password for root"
+
+mysql -h db.aviexpense.online -uroot -p${MYSQL_ROOT_PASSWd} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    echo -e "$Y Root password is not set, now setting $N"
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+    VALIDATE $? "Setting password for root"
+else
+    echo -e "$G Already Root passwd is setted $N"
+fi
